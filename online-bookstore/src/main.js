cbea9ad4e55e04ff4961948edd827508b32b4d62
malechiMLC <3054487172@qq.com>
2020-03-07 21:59:24 +0800

    upload

--- online-bookstore/src/main.js
+++ online-bookstore/src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Vue from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue';
@@ -12,19 +11,3 @@ new Vue({
   render: h => h(App),
   router
 }).$mount('#app')
-=======
-import Vue from 'vue'
-import App from './App.vue'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.less'
-import router from './router'
-
-Vue.config.productionTip = false
-
-Vue.use(Antd)
-
-new Vue({
-  render: h => h(App),
-  router
-}).$mount('#app')
->>>>>>> 7977ecb18decaadc1817aeb4317a9efbdc21229f
