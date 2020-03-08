cc4e6819c2b89b6db9c9c03dbff9b18b3d7800e5
malechiMLC <3054487172@qq.com>
2020-03-08 13:14:48 +0800

    update

--- online-bookstore/src/components/Home.vue
+++ online-bookstore/src/components/Home.vue
@@ -1,30 +1,59 @@
 <template>
-  <a-layout>
-    <a-layout-sider style="background-color:#ECECEC;" width="100px">
-    </a-layout-sider>
-    <!-- header -->
-    <a-layout>
-      <Header />
-
-      <!-- 中部内容 -->
+  <a-layout style="background-color: #ececec;">
+    <a-layout style="background-color: #ececec;">
+      <a-layout-sider style="background-color: #ececec;"
+                      width="100px">
+      </a-layout-sider>
+      <!-- header -->
       <a-layout>
-        <a-layout-sider style="background-color:white;">
-          <!-- 左侧边栏 --><SubMenu />
-        </a-layout-sider>
-        <a-layout-content> <!-- 走马灯 --><Carousel /> </a-layout-content>
-        <!-- sider -->
-        <a-layout-sider width="300px"></a-layout-sider>
+        <Header />
+
+        <!-- 中部内容 -->
+        <a-layout style="background-color: #ececec;">
+          <a-layout-sider style="background-color:white;">
+            <!-- 左侧边栏 -->
+            <SubMenu />
+          </a-layout-sider>
+          <a-layout-content style="background-color: #ececec;">
+            <!-- 走马灯 -->
+            <Carousel />
+          </a-layout-content>
+          <!-- 热书推荐
+          <a-layout-sider style="background-color: #ececec;"
+                          width="300px">
+            <List />
+          </a-layout-sider> -->
+        </a-layout>
+
+        <a-layout>
+          <a-layout-content style="background-color: #ececec;">
+            <!-- 卡片 -->
+            <Card />
+            <!-- 分割图片 -->
+            <PicDivider />
+            <!-- 卡片 -->
+            <Card />
+          </a-layout-content>
+        </a-layout>
+
+        <a-layout>
+          <a-layout-content style="background-color: #ececec;">
+            <!-- 分割图片 -->
+            <PicDivider />
+            <!-- 推荐 -->
+            <Recommend />
+          </a-layout-content>
+        </a-layout>
+
       </a-layout>
 
-      <a-layout>
-        <a-layout-content> <!-- 卡片 --><Card /> </a-layout-content>
-      </a-layout>
-
-      <a-layout-footer>Footer</a-layout-footer>
+      <a-layout-sider style="background-color: #ececec;"
+                      width="100px">
+      </a-layout-sider>
     </a-layout>
-
-    <a-layout-sider style="background-color:#ECECEC;" width="100px">
-    </a-layout-sider>
+    <a-layout-footer style="height:300px;background-color:white;margin-top:50px;">
+      <a-divider><span style="letter-spacing:4px;color:#999999;">让每个人都能享受阅读的乐趣</span></a-divider>
+    </a-layout-footer>
   </a-layout>
 </template>
 
@@ -35,13 +64,19 @@ import Carousel from '@/components/home/Carousel'
 import Card from '@/components/home/Card'
 import SubMenu from '@/components/home/SubMenu'
 import Header from '@/components/home/Header'
+import PicDivider from '@/components/home/PicDivider'
+import Recommend from '@/components/home/Recommend'
+// import List from '@/components/home/List'
 
 export default {
   components: {
     Carousel,
     Card,
     SubMenu,
-    Header
+    Header,
+    PicDivider,
+    Recommend,
+    // List
   },
 
   methods: {}
@@ -49,5 +84,5 @@ export default {
 </script>
 
 <style scope>
-@import url('../assets/css/homepage.css');
+@import url("../assets/css/homepage.css");
 </style>
---
+++ online-bookstore/src/components/home/Recommend.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex-column">
+    <div style="display:flex;justify-content:flex-start;margin-top:40px;"
+         class="ver-center"><span class="card-title-text">最热推荐</span>
+    </div>
+
+    <a-card :bordered="false"
+            style="width: 1200px;"
+            hoverable>
+      <template>
+        <div class="flex-row ver-center">
+          <div class="img-cover-div"><img src="../../assets/imgs/pic2.jpg"
+                 slot="cover"
+                 class="img-cover"></div>
+          <a-card-meta class="rec-content"
+                       align="left">
+            <template slot="description">
+              <span style="letter-spacing: 2px;font-weight:900;color:black;line-height:27px;">小王子</span><br />
+              <span style="letter-spacing: 2px;color:#555555;line-height:20px;">作者：[法] 安托万·圣·埃克苏佩里</span><br />
+              <!-- 评分 -->
+              <a-rate v-model='value'
+                      disabled="true" />
+              <br /><br />
+              <p align="left"
+                 style="line-height:20px;letter-spacing: 2px;"> 《小王子》是法国作家安托万·德·圣·埃克苏佩里于1942年写成的著名儿童文学短篇小说。本书的主人公是来自外星球的小王子。
+                <br /> <br />书中以一位飞行员作为故事叙述者，讲述了小王子从自己星球出发前往地球的过程中，所经历的各种历险。作者以小王子的孩子式的眼光，透视出成人的空虚、盲目，愚妄和死板教条，用浅显天真的语言写出了人类的孤独寂寞、没有根基随风流浪的命运。同时，也表达出作者对金钱关系的批判，对真善美的讴歌。
+                <br /> <br />小说叙述者是个飞行员，他在故事一开始告诉读者，他在大人世界找不到一个说话投机的人，因为大人都太讲实际了。
+                <br /> <br />接着，飞行员讲了六年前他因飞机故障迫降在撒哈拉沙漠遇见小王子故事。神秘的小王子来自另一个星球。飞行员讲了小王子和他的玫瑰的故事。小王子为什么离开自己的星球；在抵达地球之前，他又访问过哪些星球。他转述了小王子对六个星球的历险，他遇见了国王、爱虚荣的人、酒鬼、商人、点灯人、地理学家、蛇、三枚花瓣的沙漠花、玫瑰园、扳道工、商贩、狐狸以及我们的叙述者飞行员本人。
+                飞行员和小王子在沙漠中共同拥有过一段极为珍贵的友谊。当小王子离开地球时，飞行员非常悲伤。他一直非常怀念他们共度的时光。他为纪念小王子写了这部小说。
+                <p>
+                  <div></div>
+            </template>
+          </a-card-meta>
+        </div>
+      </template>
+
+    </a-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: 2,
+    }
+  },
+}
+</script>
+
+<style scoped>
+@import url("../../assets/css/homepage.css");
+</style>
