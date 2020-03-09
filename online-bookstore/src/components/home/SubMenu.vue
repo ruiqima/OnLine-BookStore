d61c6376049ba9a62cc0b15bbff8181ff79aa097
malechiMLC <3054487172@qq.com>
2020-03-09 15:06:19 +0800

    修改Home左侧SubMenu

--- online-bookstore/src/components/home/SubMenu.vue
+++ online-bookstore/src/components/home/SubMenu.vue
@@ -1,48 +1,39 @@
 <template>
-  <a-menu
-    @click="handleClick"
-    :defaultOpenKeys="['sub1']"
-    :openKeys.sync="openKeys"
-    mode="inline"
-  >
-    <a-sub-menu key="sub1" @titleClick="titleClick">
-      <span slot="title"> <a-icon type="bars" /><span>全部分类</span> </span>
-      <a-menu-item key="1">文艺</a-menu-item>
-      <a-menu-item key="2">心理</a-menu-item>
-      <a-menu-item key="3">社会</a-menu-item>
-      <a-menu-item key="4">法律</a-menu-item>
-      <a-menu-item key="5">科学</a-menu-item>
-      <a-menu-item key="6">科幻</a-menu-item>
-      <a-menu-item key="7">健康</a-menu-item>
-      <a-menu-item key="8">设计</a-menu-item>
-    </a-sub-menu>
-  </a-menu>
+
+  <a-list bordered
+          :dataSource="categories">
+    <a-list-item slot="renderItem"
+                 slot-scope="cate">
+      <a-button style="border:none;">{{cate.category}}
+        <a-icon type="right" />
+      </a-button>
+    </a-list-item>
+    <div slot="header">全部分类</div>
+  </a-list>
+
 </template>
 
 <script>
 export default {
-  data() {
+  data () {
     return {
-      current: ['mail'],
-      openKeys: ['sub1']
-    }
+      categories: [
+        {          categoryId: 1,
+          category: '文学'        },
+        {          categoryId: 2,
+          category: '艺术'        },
+        {          categoryId: 3,
+          category: '科学'        },
+        {          categoryId: 4,
+          category: '健康'        },
+        {          categoryId: 5,
+          category: '生活'        },
+        {          categoryId: 6,
+          category: '运动'        },
+      ]
+    };
   },
-  methods: {
-    watch: {
-      openKeys(val) {
-        console.log('openKeys', val)
-      }
-    },
 
-    // 侧面导航栏方法
-    handleClick: function(e) {
-      console.log('click', e)
-    },
-    titleClick: function(e) {
-      //点击：全部分类
-      console.log('titleClick', e)
-    }
-  }
 }
 </script>
 
