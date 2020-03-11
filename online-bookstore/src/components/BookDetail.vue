05a5648ac771b494d5c97e79dbecbdd00580e1a5
malechiMLC <3054487172@qq.com>
2020-03-11 22:28:32 +0800

    update

--- online-bookstore/src/components/BookDetail.vue
+++ online-bookstore/src/components/BookDetail.vue
@@ -1,8 +1,7 @@
 <template>
   <a-layout style="background-color: #F5F5F5;">
     <a-layout style="background-color: #F5F5F5;">
-      <a-layout-sider style="background-color: #F5F5F5;"
-                      width="7vw">
+      <a-layout-sider style="background-color: #F5F5F5;" width="7vw">
       </a-layout-sider>
       <!-- header -->
       <a-layout>
@@ -16,28 +15,31 @@
         </a-layout>
 
         <a-layout>
-          <a-layout-sider style="background-color: white;"
-                          width="7vw">
+          <a-layout-sider style="background-color: white;" width="7vw">
           </a-layout-sider>
           <a-layout-content style="background-color: white;">
             <!-- 显示评论 -->
             <Comments />
+            <div style="margin-bottom:50px;"></div>
 
-            <a-layout-sider style="background-color: white;"
-                            width="7vw">
+            <a-layout-sider style="background-color: white;" width="7vw">
             </a-layout-sider>
           </a-layout-content>
-          <a-layout-sider style="background-color: white;"
-                          width="7vw">
+          <a-layout-sider style="background-color: white;" width="7vw">
           </a-layout-sider>
         </a-layout>
       </a-layout>
-      <a-layout-sider style="background-color: #F5F5F5;"
-                      width="7vw">
+      <a-layout-sider style="background-color: #F5F5F5;" width="7vw">
       </a-layout-sider>
     </a-layout>
-    <a-layout-footer style="height:300px;background-color:white;margin-top:3.5vw;">
-      <a-divider><span style="letter-spacing:4px;color:#999999;">让每个人都能享受阅读的乐趣</span></a-divider>
+    <a-layout-footer
+      style="height:300px;background-color:white;margin-top:3.5vw;"
+    >
+      <a-divider
+        ><span style="letter-spacing:4px;color:#999999;"
+          >让每个人都能享受阅读的乐趣</span
+        ></a-divider
+      >
     </a-layout-footer>
   </a-layout>
 </template>
@@ -49,12 +51,13 @@ import Comments from '@/components/detail/Comments'
 
 export default {
   components: {
-
-    Header, DetailCard, Comments
-  },
+    Header,
+    DetailCard,
+    Comments
+  }
 }
 </script>
 
 <style scoped>
-@import url("../assets/css/homepage.css");
+@import url('../assets/css/homepage.css');
 </style>
--- online-bookstore/src/components/detail/Comments.vue
+++ online-bookstore/src/components/detail/Comments.vue
@@ -1,84 +1,79 @@
 <template>
   <div>
-
-    <a-list class="comment-list"
-            style="text-align:left;font-size:1vw;"
-            :header="`所有评价 ${data.length}`"
-            itemLayout="horizontal"
-            :pagination="pagination"
-            :dataSource="data">
-      <a-list-item slot="renderItem"
-                   slot-scope="item">
+    <a-list
+      class="comment-list"
+      style="text-align:left;font-size:1vw;"
+      :header="`所有评价 ${data.length}`"
+      itemLayout="horizontal"
+      :pagination="pagination"
+      :dataSource="data"
+    >
+      <a-list-item slot="renderItem" slot-scope="item">
         <!-- 用户头像，昵称 -->
-        <a-comment :author="item.author"
-                   :avatar="item.profile">
+        <a-comment :author="item.author" :avatar="item.profile">
           <!-- 评分 -->
-          <a-rate v-model='item.stars'
-                  disabled />
-          <span class="ant-rate-text">{{item.stars}} 星</span>
+          <a-rate v-model="item.stars" disabled />
+          <span class="ant-rate-text">{{ item.stars }} 星</span>
           <!-- 评论具体内容 -->
-          <p slot="content">{{item.content}}</p>
+          <p slot="content">{{ item.content }}</p>
           <!-- 评论时间 -->
-          <span slot="datetime"
-                style="margin-left:100px;">{{item.time | formatDate}}</span>
+          <span slot="datetime" style="margin-left:100px;">{{
+            item.time | formatDate
+          }}</span>
         </a-comment>
       </a-list-item>
     </a-list>
-
   </div>
-
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       pagination: {
         onChange: page => {
           alert(page)
         },
-        pageSize: 1,
+        pageSize: 1
       },
       data: [
         {
           author: '小小小王子',
-          profile: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+          profile:
+            'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
           content: '我最喜欢小王子这本书了',
           stars: 2,
-          time: 1583500980000,
+          time: 1583500980000
         },
         {
           author: '我的愿望是拥有一只博美然后一只大金毛',
-          profile: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+          profile:
+            'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
           content: '小王子和玫瑰花，那小狐狸和飞行员呢？',
           stars: 3,
-          time: 1583500980000,
-        },
-      ],
+          time: 1583500980000
+        }
+      ]
     }
   },
   filters: {
-    formatDate: function (value) {
-      let date = new Date(value);
-      let y = date.getFullYear();
-      let MM = date.getMonth() + 1;
-      MM = MM < 10 ? ('0' + MM) : MM;
-      let d = date.getDate();
-      d = d < 10 ? ('0' + d) : d;
-      let h = date.getHours();
-      h = h < 10 ? ('0' + h) : h;
-      let m = date.getMinutes();
-      m = m < 10 ? ('0' + m) : m;
-      let s = date.getSeconds();
-      s = s < 10 ? ('0' + s) : s;
-      return y + '-' + MM + '-' + d + ' ' + h + ':' + m + ':' + s;
+    formatDate: function(value) {
+      let date = new Date(value)
+      let y = date.getFullYear()
+      let MM = date.getMonth() + 1
+      MM = MM < 10 ? '0' + MM : MM
+      let d = date.getDate()
+      d = d < 10 ? '0' + d : d
+      let h = date.getHours()
+      h = h < 10 ? '0' + h : h
+      let m = date.getMinutes()
+      m = m < 10 ? '0' + m : m
+      let s = date.getSeconds()
+      s = s < 10 ? '0' + s : s
+      return y + '-' + MM + '-' + d + ' ' + h + ':' + m + ':' + s
     }
   }
 }
 </script>
 
-<style scoped>
-</style>
-
-
-
+<style scoped></style>
